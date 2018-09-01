1d7779d109748bdc87374ef0633cbf69fb770bf6
lorio <lori@loriortiz.com>
2018-09-01 11:31:32 -0400

    call functions

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const counter2 = document.createElement('p');
 const card2 = document.getElementById('card2');
 card2.appendChild(counter2);
 let likes2 = 0;
-counter2.textContent = `${likes2}`;
+counter2.textContent = `${name} has ${likes2} likes.`;
 
 /*counter.textContent = '1';*/
 class Cat {
   constructor(name = 'lou', likes = 0, card = 'card1') {
-    this.name = name;
+    this.name = 'name';
     this.likes = 0;
     this.card = 'card';
     this.pic = 'pic';
@@ -24,42 +24,17 @@ class Cat {
     }
   buildProfile() {
     /*let cards = [card1, card2].map(card => {*/
-      this.pic = document.querySelector('img');
+      let pic = document.querySelector('img');
       pic.addEventListener('click', function() {
-      likes++;             
+      this.likes++;             
       });
   }
 }
-/*cat = new Cat;
-cat.buildProfile(
-  name = 'lou', 
-  likes = 0, 
-  pic = 'img/cat.jpg', 
-  card = 'card[0]'
-  );*/
-/*let allCats.forEach(cat => {
-
-});
-let allCats = [...Array(2)].map((cat,name,pic) =>  new Cat(name,pic));
-  cat.buildProfile();
-*/
 
 let cat1 = new Cat('lou', 0, 'card1', 'img/cat.jpg' );
-let cat2 = new Cat('lily',0, 'card2', 'img/cat2.jpg');
+let cat2 = new Cat('lily', 0, 'card2', 'img/cat2.jpg');
+cat1.getCaption();
+cat1.buildProfile();
+cat2.getCaption();
+cat2.buildProfile();
 
-
-
-/*allCats.forEach(cat) {
-articleHead1.appendChild(counter);
-const pic1 = document.querySelector('.one img');
-pic1.addEventListener('click', function() {
-  likes++;
-  counter.textContent = `${likes}`;
-});
-}
-articleHead2.appendChild(counter);
-const pic2 = document.querySelector('.two img');
-pic2.addEventListener('click', function() {
-  likes++;
-  counter.textContent = `${likes}`;
-});*/
